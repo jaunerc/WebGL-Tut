7819e0ad154609e0f80741bc06b3400576495171
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2018-12-30 11:02:20 +0100

    reorg tutorial 3

--- tutorial-03/Drawing.js
+++ tutorial-03/Drawing.js
@@ -1,5 +1,12 @@
-
+/**
+ * This class represents a component that can be drawn.
+ */
 class Drawable {
+    /**
+     * Creates a new Drawable object.
+     * @param color RGB color as list [r, g, b]
+     * @param position 2d vector {x:, y:}
+     */
     constructor(color, position) {
         this.color = color;
         this.position = position;
@@ -10,6 +17,9 @@ class Drawable {
     }
 }
 
+/**
+ * This class represents an empty rectangle with a solid border.
+ */
 class RectangleLines extends Drawable {
     constructor(gl, color, position, width, height) {
         super(color, position);
@@ -22,48 +32,20 @@ class RectangleLines extends Drawable {
         this.rectangleBuffer = new RectangleBuffer(gl);
     }
 
-    createModelMat() {
-        let modelMat = mat3.create();
-        mat3.fromTranslation(modelMat, this.position);
-        mat3.scale(modelMat, modelMat,[this.width, this.height]);
-        return modelMat;
+    /**
+     * Creates the modelview-matrix for this rectangle.
+     * @returns {mat3} movelview-matrix
+     */
+    createModelViewMat() {
+        let modelViewMat = mat3.create();
+        mat3.fromTranslation(modelViewMat, this.position);
+        mat3.scale(modelViewMat, modelViewMat,[this.width, this.height]);
+        return modelViewMat;
     }
 
     draw(gl, aVertexPositionId, uColorId, uModelMatId) {
         gl.uniform3fv(uColorId, this.color);
-        gl.uniformMatrix3fv(uModelMatId, false, this.createModelMat());
+        gl.uniformMatrix3fv(uModelMatId, false, this.createModelViewMat());
         this.rectangleBuffer.draw(gl, aVertexPositionId);
     }
-}
-
-function createRectangles(numRects, posRange) {
-    let rectangles = [];
-    let maxSize = 100;
-    for (let i = 0; i < numRects; i++) {
-        let color = {r: Math.random(), g: Math.random(), b: Math.random()};
-        let position = randomPosition(posRange);
-        let width = randomInt(maxSize);
-        let height = randomInt(maxSize);
-        rectangles.push(new RectangleLines(gl, [color.r, color.g, color.b], position, width, height));
-    }
-    return rectangles;
-}
-
-function randomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-function randomPosition(posRange) {
-    let position = [randomInt(posRange.x), randomInt(posRange.y)];
-    if (coinFlip()) {
-        position[0] *= -1;
-    }
-    if (coinFlip()) {
-        position[1] *= -1;
-    }
-    return position;
-}
-
-function coinFlip() {
-    return Math.random() > 0.5;
 }
--- tutorial-03/tutorial-03.js
+++ tutorial-03/tutorial-03.js
@@ -77,3 +77,55 @@ function draw() {
         rect.draw(gl, context.aVertexPositionId, context.uColorId, context.uModelMatId);
     })
 }
+
+/**
+ * Creates rectangles with random properties.
+ * @param numRects The number of rectangles
+ * @param posRange Min and max position as 2d vector {x:, y:}
+ * @returns {Array} List of rectangles.
+ */
+function createRectangles(numRects, posRange) {
+    let rectangles = [];
+    let maxSize = 100;
+    for (let i = 0; i < numRects; i++) {
+        let color = {r: Math.random(), g: Math.random(), b: Math.random()};
+        let position = randomPosition(posRange);
+        let width = randomInt(maxSize);
+        let height = randomInt(maxSize);
+        rectangles.push(new RectangleLines(gl, [color.r, color.g, color.b], position, width, height));
+    }
+    return rectangles;
+}
+
+/**
+ * Gets a random integer in the interval [0, max -1].
+ * @param max Upper bound
+ * @returns {number} A random integer
+ */
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * Gets a random position.
+ * @param posRange The range for the position [-x,x] [-y,y]
+ * @returns {number[]} A random position as list [x, y]
+ */
+function randomPosition(posRange) {
+    let position = [randomInt(posRange.x), randomInt(posRange.y)];
+    if (coinFlip()) {
+        position[0] *= -1;
+    }
+    if (coinFlip()) {
+        position[1] *= -1;
+    }
+    return position;
+}
+
+/**
+ * Gets a random boolean.
+ * @returns {boolean} A random boolean
+ */
+function coinFlip() {
+    return Math.random() > 0.5;
+}
