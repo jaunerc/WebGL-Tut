bbc958f77715491d06214f489385f7b24ed6872b
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-01-10 13:14:27 +0100

    lighting completed

--- tutorial-10/FragmentShader.glsl
+++ tutorial-10/FragmentShader.glsl
@@ -13,6 +13,8 @@ varying vec3 vVertexPositionEye3;
 varying vec2 vTextureCoord;
 
 const float ambientFactor = 0.2;
+const float diffuseFactor = 1.0;
+const float shininess = 64.0;
 
 void main() {
     vec3 baseColor = vColor;
@@ -21,12 +23,51 @@ void main() {
     }
 
     if (uEnableLighting) {
-        vec3 color = baseColor;
+        vec3 color;
+        vec3 directionToLight;
+        vec3 vertexNormal;
+        vec3 reflection;
+        vec3 eyeDirection;
+        vec3 ambientColor;
+        vec3 diffuseColor;
+        vec3 specularColor;
+        float cosPhi;
+
+        color = baseColor;
+
+        // calculates the vector from the current vertex to the light source
+        directionToLight = normalize(uLightPosition - vVertexPositionEye3);
+
+        // normalizes the vertex normal vector
+        vertexNormal = normalize(vNormalEye);
 
         // ambient lighting
-        color = ambientFactor * color;
+        ambientColor = ambientFactor * color;
 
-        gl_FragColor = vec4(color, 1);
+        // diffuse lighting
+        cosPhi = dot(vNormalEye, directionToLight);
+        cosPhi = clamp(cosPhi, 0.0, 1.0);
+        diffuseColor = diffuseFactor * cosPhi * color;
+
+        // specular lighting
+        // calculate reflection
+        reflection = 2.0 * (dot(vertexNormal, directionToLight)) * vertexNormal - directionToLight;
+        reflection = normalize(reflection);
+
+        // calculate the direction to the camera position
+        eyeDirection = -normalize(vVertexPositionEye3);
+        cosPhi = dot(reflection, eyeDirection);
+        cosPhi = clamp(cosPhi, 0.0, 1.0);
+        cosPhi = pow(cosPhi, shininess);
+
+        if (cosPhi > 0.0) {
+            specularColor = uLightColor * cosPhi;
+            diffuseColor = diffuseColor * (1.0 - cosPhi);
+        } else {
+            specularColor = vec3(0.0, 0.0, 0.0);
+        }
+
+        gl_FragColor = vec4(ambientColor + diffuseColor + specularColor, 1);
     } else {
         gl_FragColor = vec4(baseColor, 1.0);
     }
