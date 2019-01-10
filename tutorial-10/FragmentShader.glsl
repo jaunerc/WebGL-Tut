d571ea3b9167650cab7dd0d52fb5daf5857cc8bb
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-01-10 09:56:54 +0100

    ambient lighting added

--- tutorial-10/FragmentShader.glsl
+++ tutorial-10/FragmentShader.glsl
@@ -12,6 +12,8 @@ varying vec3 vNormalEye;
 varying vec3 vVertexPositionEye3;
 varying vec2 vTextureCoord;
 
+const float ambientFactor = 0.2;
+
 void main() {
     vec3 baseColor = vColor;
     if (uIsTextureDrawing) {
@@ -19,8 +21,13 @@ void main() {
     }
 
     if (uEnableLighting) {
+        vec3 color = baseColor;
 
+        // ambient lighting
+        color = ambientFactor * color;
+
+        gl_FragColor = vec4(color, 1);
+    } else {
+        gl_FragColor = vec4(baseColor, 1.0);
     }
-
-    gl_FragColor = vec4(baseColor, 1.0);
 }
